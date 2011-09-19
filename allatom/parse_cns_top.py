ee69f9fb8e44020584638884e70b64a36a8bad89
sjoerd <sjoerd@sjoerd-desktop>
2011-09-19 10:08:07 +0200

    ensemble docking

---
+++ allatom/parse_cns_top.py
@@ -0,0 +1,179 @@
+#CNS parameter file parser, May 2011, Sjoerd de Vries
+#Reads in residue and residue patch (presidue) statements
+#Currently, all but atom-related statements are ignored
+#Note: +C and -C modifiers are ignored! 
+
+allspecies = ("atom", "angle", "bond", "improper","impr","dono", "donor", "acce", "acceptor", "dihed", "dihedral")
+
+def tokenize(s):
+  s = s.lower()
+  ss = s.split()
+  r = []
+  for t in ss:
+    pos = t.find("=")
+    while pos > -1:
+      r.append(t[:pos])
+      r.append("=")
+      t = t[pos+1:]
+      pos = t.find("=")
+    r.append(t)
+  ret = []
+  for rr in r:
+    if rr.startswith("!"): break
+    if len(rr) == 0: continue
+    ret.append(rr)
+  return ret
+
+class Atom(object):
+  def __init__(self, name, type, charge):
+    self.name = name.lstrip("-").lstrip("+")
+    self.type = type
+    self.charge = charge
+  def copy(self):
+    return type(self)(self.name, self.type, self.charge)
+  def modify(self, a):
+    if a.name is not None: self.name = a.name
+    if a.type is not None: self.type = a.type
+    if a.charge is not None: self.charge = a.charge    
+  @staticmethod
+  def _parse(tokens):
+    name = tokens[0]
+    d = {"type":None,"charge":None}
+    assert tokens[1] in ("type","charge"), tokens
+    assert tokens[2] == "=", tokens
+    d[tokens[1]] = tokens[3]
+    if len(tokens) > 4:
+      assert tokens[4] in ("type","charge"), tokens
+      assert tokens[5] == "=", tokens
+      d[tokens[4]] = tokens[6]
+    if d["charge"] is not None: d["charge"] = float(d["charge"])
+    return name, d["type"], d["charge"]
+  @classmethod
+  def parse(cls, tokens):
+    name, type, charge = cls._parse(tokens)
+    assert type is not None and charge is not None, (type, charge)
+    return cls(name, type, charge)
+  @classmethod
+  def partparse(cls, tokens):
+    name, type, charge = cls._parse(tokens)
+    return cls(name, type, charge)
+  def __str__(self):
+    return "atom %s type=%s charge=%.6f" % (self.name, self.type, self.charge)
+        
+class Residue(object):
+  def __init__(self, name):
+    self.name = name
+    self.atomorder = []
+    self.atoms = {}
+  def add(self, species,tokens):
+    assert species in allspecies, species
+    if species != "atom": return
+    assert tokens[-1] == "end", tokens[-1]
+    a = Atom.parse(tokens[:-1])
+    self.atomorder.append(a.name)
+    self.atoms[a.name] = a
+  def delete(self, species, name):
+    assert species in allspecies
+    if species != "atom": return
+    #assert name in self.atoms, name
+    try:
+      del self.atoms[name]
+      self.atomorder.remove(name)
+    except KeyError: 
+      pass
+  def modify(self, species, a):
+    assert species in allspecies
+    if species != "atom": return
+    assert a.name in self.atoms, a.name
+    self.atoms[a.name].modify(a)
+  def copy(self):
+    ret = type(self)(self.name)
+    ret.atomorder = list(self.atomorder)
+    for aname in self.atoms:
+      ret.atoms[aname] = self.atoms[aname].copy()
+    return ret
+  def patch(self, pres):
+    for mode, species, obj in pres.commands:
+      if species != "atom": continue
+      if mode == "add":
+        self.atomorder.append(obj.name)
+        self.atoms[obj.name] = obj
+      elif mode == "modify":
+        self.modify(species, obj)
+      elif mode == "delete":
+        self.delete(species, obj)
+      else: raise ValueError(mode)
+    
+
+class PResidue(object):
+  def __init__(self, name):
+    self.name = name
+    self.commands = []
+  def add_command(self,mode,tokens):
+    species = tokens[0]
+    assert species in allspecies
+    if species != "atom": return
+    if mode == "dele": mode = "delete"
+    if mode == "modi": mode = "modify"
+    if mode == "add":
+      a = Atom.parse(tokens[1:])
+      self.commands.append((mode, species, a))
+    elif mode == "modify":
+      assert tokens[-1] == "end"
+      a = Atom.partparse(tokens[1:-1])
+      self.commands.append((mode, species, a))
+    elif mode == "delete":
+      assert tokens[-1] == "end", tokens
+      assert len(tokens) == 3
+      name = tokens[1].lstrip("-").lstrip("+")
+      self.commands.append((mode, species, name))
+    else:
+      raise ValueError("unknown mode: %s", mode)
+
+def parse_stream(stream): 
+  residues = {}
+  presidues = {}
+  mode = 0
+  linenr = 0
+  for line in stream:
+    linenr += 1
+    l = line.rstrip("\n").rstrip("\r")
+    tokens = tokenize(l)
+    if len(tokens) == 0: continue
+    if mode == 0:
+      if tokens[0] in ("residue", "resi", "resid"): 
+        name = tokens[1]
+        res = Residue(name)
+        residues[name] = res
+	mode = 1; continue
+      elif tokens[0] in ("presidue", "pres", "presi","presid"):
+        name = tokens[1]
+        res = PResidue(name)
+	presidues[name] = res
+        mode = 2; continue      
+      else: continue
+    elif mode == 1:
+      if tokens[0] != "group": continue
+      mode = 2; continue
+    if tokens[0] == "end":
+      mode = 0; continue
+    #print linenr, tokens
+    typename = res.__class__.__name__
+    if typename == "Residue":
+      res.add(tokens[0],tokens[1:])
+    elif typename == "PResidue":
+      if tokens[0] == "group": continue
+      res.add_command(tokens[0], tokens[1:])
+      
+    #print res.__class__.__name__, name, tokens
+  return residues, presidues
+    
+    
+if __name__ == "__main__":
+  residues, presidues = parse_stream(open("topallhdg5.3.pro"))      
+  print residues.keys()    
+  a = residues["ala"].copy()
+  for atom in a.atoms: print a.atoms[atom]
+  print  
+  a.patch(presidues["cter"])
+  for atom in a.atoms: print a.atoms[atom]
---
+++ allatom/reduce.py
@@ -0,0 +1,109 @@
+break_threshold = 3.5 #3.5 A before a N/C-terminal patch is applied
+threshsq = break_threshold * break_threshold
+
+import sys
+
+import parse_cns_top 
+
+pdb = sys.argv[1]
+transfile = sys.argv[2]
+topfile = sys.argv[3]
+
+residues, presidues = parse_cns_top.parse_stream(open(topfile))
+
+code_to_type = {}
+for l in open(transfile):
+  ll = l.split()
+  type = int(ll[0])
+  for code in ll[3:]:
+    if code.startswith("#"): break
+    assert code not in code_to_type, code
+    code_to_type[code] = type
+
+class PDBres:
+  def __init__(self, resid, resname):
+    self.resid = resid
+    self.resname = resname
+    self.coords = {} 
+    self.ter = False
+
+pdbres = []
+
+rescounter = 1
+atomcounter = 1
+def write_res(res, resprev=None, resnext=None):
+  global rescounter, atomcounter
+  
+  patchn = True
+  if resprev is not None and resprev.resid[0] == res.resid[0] \
+   and resprev.ter == False: 
+    if "C" in resprev.coords and "N" in res.coords:
+      x1,y1,z1 = resprev.coords["C"]
+      x2,y2,z2 = res.coords["N"]
+      d = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+      if d < threshsq: patchn = False
+
+  patchc = True
+  if resnext is not None and resnext.resid[0] == res.resid[0] \
+   and res.ter == False: 
+    if "C" in res.coords and "N" in resnext.coords:
+      x1,y1,z1 = res.coords["C"]
+      x2,y2,z2 = resnext.coords["N"]
+      d = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+      if d < threshsq: patchc = False
+
+  r = residues[res.resname.lower()].copy()
+  if patchn:
+    r.patch(presidues["nter"])
+  if patchc:
+    r.patch(presidues["cter"])
+
+  done = set()
+  for a in r.atomorder: 
+    atom = r.atoms[a]
+    if a.startswith("h") and atom.charge == 0: continue
+    aa = a.upper()
+    x = " XXXXXXX"
+    y = x; z = x
+    if aa.strip() in res.coords:
+      x,y,z = ("%8.3f" % v for v in res.coords[aa.strip()])
+    xyz = x + y + z
+    type = code_to_type[atom.type.upper()]
+    a0 = aa
+    if len(a0) < 4:
+      a0 = " " + a0 + "   "[len(a0):]
+    print "ATOM %6d %4s %s %5d    %s %4d %6.3f 0 1.00" % \
+     (atomcounter, a0, res.resname, rescounter, xyz, type, atom.charge )
+    atomcounter += 1
+  rescounter += 1
+
+
+curr_res = None
+lines = open(pdb).readlines()
+    
+for l in lines:
+  if l.startswith("TER"):
+    if curr_res is not None: curr_res.ter = True
+    continue
+  if l.startswith("ATOM"):
+    atomcode = l[12:16].strip()
+    assert l[16] == " ", l
+    resname = l[17:20]   
+    resid = l[21:27]
+    x = float(l[30:38])
+    y = float(l[38:46])
+    z = float(l[46:54])
+    if curr_res is None \
+     or (resid != curr_res.resid) or resname != curr_res.resname:
+      curr_res = PDBres(resid, resname)
+      pdbres.append(curr_res)
+    curr_res.coords[atomcode] = (x,y,z)
+
+for n in range(len(pdbres)):
+  res = pdbres[n]
+  resprev = None
+  if n > 0: resprev = pdbres[n-1]
+  resnext = None
+  if n < len(pdbres)-1: resnext = pdbres[n+1]
+  write_res(res, resprev, resnext)
+
