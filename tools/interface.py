5a4a3c2c4734a69b42050208013e8e6c14228ee4
Christina Schindler <christina.schindler@tum.de>
2014-03-07 15:02:38 +0100

    small changes

--- tools/interface.py
+++ tools/interface.py
@@ -69,8 +69,6 @@ from scipy.spatial.distance import cdist
 def get_interface(a1, a2, output,output2,rcut):
     ilist1 = []
     ilist2 = []
-    contactlist = []
-    distdata = {}
     crd1 = [[x,y,z] for name, res, x,y,z in a1]
     crd1 = np.matrix(crd1)
     crd2 = [[x,y,z] for name, res, x,y,z in a2]
@@ -104,7 +102,28 @@ def get_interface(a1, a2, output,output2,rcut):
             out2.write(str(i)+'\n')
             
     out2.close()
-        
+
+def get_contacts(structure,rcut=5.0):
+    a1, a2 = read_struc(structure)
+    contacts = []
+    ncontact = 0
+    crd1 = [[x,y,z] for name, res, x,y,z in a1]
+    crd1 = np.matrix(crd1)
+    crd2 = [[x,y,z] for name, res, x,y,z in a2]
+    crd2 = np.matrix(crd2)
+    Y = cdist(crd1,crd2,'euclidean')
+    for i in range(len(a1)):
+      res1 = a1[i][1]
+      for j in range(len(a2)):
+	res2 = a2[j][1]
+	dist = Y[i][j]
+	if dist < rcut:
+	  if not (res1,res2) in contacts:
+	    contacts.append((res1,res2))
+	    ncontact += 1
+	    
+    print ncontact
+	  
 def get_interface2(a1, a2, output,ouput2,rcut):
     ilist = []
     while len(ilist) < 8:
@@ -138,27 +157,82 @@ def make_interfacelist(file1, file2, directory,rcut=3.0,name1='rlist',name2='lli
     a1 = read_file(file1)
     a2 = read_file(file2)
     get_interface(a1, a2, directory+'/'+name1+'.txt',directory+'/'+name2+'.txt',rcut)
-    #if not name1 == 'rlist':
-        #import restraints_from_topology as resttop
-        #import numpy as np
-        #ilist = np.loadtxt(directory+'/'+name1+'.txt',dtype=int)
-        #atomlist, pdb2, pdbid = resttop.make_interfacelist(ilist,file1)
-        #outfile = directory+'/'+name1+'-aa.txt'
-        #out = open(outfile,'w')
-        #for atom in atomlist:        
-            #out.write(str(atom)+'\n')
-            
-        #out.close()
-        #ilist = np.loadtxt(directory+'/'+name2+'.txt',dtype=int)
-        #atomlist, pdb2, pdbid = resttop.make_interfacelist(ilist,file2)
-        #outfile = directory+'/'+name2+'-aa.txt'
-        #out = open(outfile,'w')
-        #for atom in atomlist:        
-            #out.write(str(atom)+'\n')
-            
-        #out.close()
         
-
+def check_contacts(struc,ilist1old, ilist2old,directory,name):
+    a1, a2 = read_struc(struc)
+    ilist1, ilist2 = [], []
+    crd1 = [[x,y,z] for name1, res, x,y,z in a1]
+    crd1 = np.matrix(crd1)
+    crd2 = [[x,y,z] for name1, res, x,y,z in a2]
+    crd2 = np.matrix(crd2)
+    Y = cdist(crd1,crd2,'euclidean')
+    remove = open(directory+'/'+name+'rlist-remove.txt','w')
+    ilist1new, ilist2new = collect_contacts(a1,a2,Y,5.0)
+    for res in ilist1old:
+      if not res in ilist1new:
+	remove.write(str(res)+'\n')
+      else:
+	ilist1.append(int(res))
+	
+    remove.close()
+    remove2 = open(directory+'/'+name+'llist-remove.txt','w')
+    for res in ilist2old:
+      if not res in ilist2new:
+	remove2.write(str(res)+'\n')
+	
+      else:
+	ilist2.append(int(res))
+	
+    ilist1backup = ilist1[:]
+    ilist2backup = ilist2[:]
+    remove2.close()
+    rcut = 3.0
+    while (len(ilist1) < 8 or len(ilist2) < 8) and countilist(ilist1,a1,ilist2,a2) < 333:
+      ilist1new, ilist2new = collect_contacts(a1,a2,Y,rcut)
+      for res in ilist1new:
+	if not res in ilist1:
+	  ilist1.append(res)
+	  
+      for res in ilist2new:
+	if not res in ilist2:
+	  ilist2.append(res)
+	  
+      rcut += 0.5
+     
+    while countilist(ilist1,a1,ilist2,a2) > 333:
+      rcut -= 0.5
+      ilist1new, ilist2new = collect_contacts(a1,a2,Y,rcut)
+      for res in ilist1:
+	if not res in ilist1new and not res in ilist1backup:
+	  ilist1.pop(ilist1.index(res))
+	  
+      for res in ilist2:
+	if not res in ilist2new and not res in ilist2backup:
+	  ilist2.pop(ilist2.index(res))
+	  
+	  
+    output = directory+'/'+name+'rlist.txt'
+    output2 = directory+'/'+name+'llist.txt'
+    out = open(output, 'w')
+    if len(ilist1) == 0:
+        out.write('# no flexible residues')
+        
+    else:
+        for i in ilist1:
+            out.write(str(i)+'\n')
+            
+    out.close()      
+    out2 = open(output2, 'w')
+    if len(ilist2) == 0:
+        out2.write('# no flexible residues')
+        
+    else:
+        for i in ilist2:
+            out2.write(str(i)+'\n')
+            
+    out2.close()
+    
+    
 import os    
 def make_interface(struc,directory,name,rcut=3.0):
     a1, a2 = read_struc(struc)
@@ -167,7 +241,16 @@ def make_interface(struc,directory,name,rcut=3.0):
 #Main
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 3:
+    if '--ncontact' in sys.argv:
+      get_contacts(sys.argv[1])
+      
+    elif '--continue' in sys.argv:
+      struc, fileilist1, fileilist2,directory,name = sys.argv[1:6]
+      ilist1 = list(np.loadtxt(fileilist1,dtype='int'))
+      ilist2 = list(np.loadtxt(fileilist2,dtype='int'))
+      check_contacts(struc,ilist1,ilist2,directory,name)
+      
+    elif len(sys.argv) == 3:
         struc, directory = sys.argv[1:]
         make_interface(struc, directory)
         
