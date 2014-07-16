267a6f96e03eace105710f2949af3b5296dcde9e
Sjoerd de Vries <sjdv1982@gmail.com>
2014-07-16 12:28:17 +0200

    web interface upgrade, nearly done

--- gui/server/attractserver-easy.py
+++ gui/server/attractserver-easy.py
@@ -1,23 +1,49 @@
 #!/usr/bin/env python
-  
-print "Content-type: text/html\n\n<pre>"
+from __future__ import print_function
 
-try:
-  
+print("Content-type: text/html\n\n<pre>")
+
+try:  
   import os, sys, time, traceback
   sys.path.append(os.path.split(os.path.abspath(__file__))[0] + "/..") 
   import spyder, Spyder
-  import spyder.cgimodule
+  import spyder.formtools
   import attractmodel
   import formeasy
   import attractsave
-  from serverlib import serve_attract
-
-  r = serve_attract(Spyder.AttractEasyModel, formeasy, attractsave.deploy_easy)
-  print r
+  from serverlib import serve_attract, AttractServerError
+  os.system("chmod a+r+w /tmp/*spy.*")
 except Exception, e:
-  print traceback.format_exc() + "\n"
-  print "</pre>"  
+  s = "There was an error in the initialization of the server\n"
+  s += """Please save this page from your browser as HTML and email it to sjoerd@tum.de, and it will be fixed as soon as possible.
+The ATTRACT web interface is in active development, thank you for your patience.  
+"""
+  s += "\n<B>Full error information</B>:\n\n"
+  s += traceback.format_exc()
+  print(s)
+  print("</pre>")
+  sys.exit()
+  
+try:  
+  r = serve_attract(Spyder.AttractEasyModel, formeasy, attractsave.deploy_easy)
+  print(r)
+except AttractServerError as e:
+  print("There was an inconsistency in your data")
+  print("\n<B>Error message</B>")
+  print(e.status)
+  if e.delta is not None:
+    print("\n<B>You can download the following file</B>")
+    print("\n")
+    print("\n".join(e.delta))
+  print("</pre>") 
+  sys.exit()  
+except Exception as e:
+  print("There was an unknown error in the server")
+  print("""Please save this page from your browser as HTML and email it to sjoerd@tum.de, and it will be fixed as soon as possible.
+The ATTRACT web interface is in active development, thank you for your patience.  
+""")  
+  print(traceback.format_exc() + "\n")
+  print("</pre>") 
   sys.exit()
 
-print "</pre>"  
+print("</pre>")
---
+++ gui/server/serverconfig-local.py
@@ -0,0 +1,11 @@
+###########################################
+# Change the following directory
+localserverdir = "/home/sjoerd/server"
+servicename = "ATTRACT"
+###########################################
+
+website = "http://localhost"
+webdir = website + "/server/%s/" % servicename
+webresultdir = website + "/results/server/%s/" % servicename
+localdir = localserverdir + "/%s/html/" % servicename
+localresultdir = localserverdir + "/%s/results/" % servicename
