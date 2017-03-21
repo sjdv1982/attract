f076a3eeecf2ed68e5f15be587e39163fe623062
Sjoerd de Vries <sjdv1982@gmail.com>
2017-03-21 22:06:17 +0100

    fix

--- gui/server/serverconfig-tum-devel.py
+++ gui/server/serverconfig-tum-devel.py
@@ -10,3 +10,4 @@ cgidir = website + "/cgi/services/%s/" % servicename
 webresultdir = website + "/results/services/%s/" % servicename
 localdir = localserverdir + "/%s/html/" % servicename
 localresultdir = localserverdir + "/%s/results/" % servicename
+rpbs_website="http://mobyle.rpbs.univ-paris-diderot.fr/cgi-bin"
