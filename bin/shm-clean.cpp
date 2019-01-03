c7fd09390af724052bfb5a9b10e992bb7d625fe5
Sjoerd de Vries <sjdv1982@gmail.com>
2019-01-03 23:31:10 +0100

    added generation/specification of specific shared-grid block numbers to make-grid/shm-grid/shm-clean

--- bin/shm-clean.cpp
+++ bin/shm-clean.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstring>
+#include <memory>
 #include <sys/mman.h>
+
 
 char *files[1000];
 int nrfiles;
@@ -10,14 +12,24 @@ void get_shm_name(int shm_id, char *shm_name) {
 }
 
 int main(int argc, char *argv[]) {  
-  for (int n = 0; n <= 100; n++) {
-    char *name = new char[100];
-    get_shm_name(n,name);
-    files[n] = name;
+  if (argc == 1) {
+    for (int n = 0; n <= 100; n++) {
+      char *name = new char[100];
+      get_shm_name(n,name);
+      files[n] = name;
+    }
+    nrfiles = 101;
+    for (int n = 0; n < nrfiles; n++) {
+      //printf("%s\n", files[n]);
+      shm_unlink(files[n]);
+    }
   }
-  nrfiles = 101;
-  for (int n = 0; n < nrfiles; n++) {
-    //printf("%s\n", files[n]);
-    shm_unlink(files[n]);
+  else {
+    char filename[100];
+    for (int n = 1; n < argc; n++ ) {
+      int nn = atoi(argv[n]);      
+      get_shm_name(nn,filename);
+      shm_unlink(filename);
+    }
   }
 }
